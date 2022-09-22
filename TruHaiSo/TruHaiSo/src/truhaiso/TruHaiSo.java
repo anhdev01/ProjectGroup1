4fcf33a4d519dacdb605dc60ac569000653b3e1d
Lahm Phạm <quanseven761@gmail.com>
2022-09-23 00:33:36 +0700

    PhepTru

---
+++ TruHaiSo/TruHaiSo/src/truhaiso/TruHaiSo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package truhaiso;
+
+/**
+ *
+ * @author quans
+ */
+public class TruHaiSo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     int num1 = 10, num2 = 7, hieu;
+      hieu = num1 - num2;
+ 
+      System.out.println("Hieu hai so la: "+hieu);
+   
+    }
+    
+}
